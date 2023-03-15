b7d407adcff39856ab09c4ca650d1d8f81a12981
Kauan <kauaanmatheus@gmail.com>
2023-03-14 22:14:40 -0300

    try deploy

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connecToDb = () => {
     mongoose
     .connect(
-        "mongodb+srv://root:admin@listadefilmes.fjy3csz.mongodb.net/?retryWrites=true&w=majority",
+        process.env.DB_URI,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const express = require("express");
 const path = require("path");
 const routes = require("./routs/routes");
 const connectToDb = require("./database/db");
 
-connectToDb()
-const app = express()
-const port = 10
+connectToDb();
+const app = express();
+const port = process.env.PORT;
 
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, './public')));
